c010e91deea767a9f0aeb92e46acebb19c122a05
davidspiv <daspivack@gmail.com>
2024-07-05 18:23:17 -0700

    journal mapping transactions correctly

---
+++ src/components/TransactionCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Transaction from '../models/Transaction'
+
+const props = defineProps<{
+  data: Transaction
+}>()
+
+const { memo, amount } = props.data
+</script>
+
+<template>
+  <ul>
+    <li>{{ memo }}</li>
+    <li>{{ amount }}</li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 6;
+}
+</style>
--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -1,7 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import Transaction from '../models/Transaction'
+import TransactionCard from '@/components/TransactionCard.vue'
+import type { Ref } from 'vue'
+
+const transactions: Ref<[] | Transaction[]> = ref([])
+fetchTransactions()
+
+async function fetchTransactions() {
+  const formattedData: Transaction[] = []
+  try {
+    const apiUrl = 'http://localhost:5000/api/transactions/?_limit=6'
+    const res = await fetch(apiUrl)
+    const data = await res.json()
+    for (const trans of data.transactions) {
+      formattedData.push(
+        new Transaction(
+          trans.id,
+          trans.date,
+          trans.dateOffset,
+          trans.amount,
+          trans.memo,
+          trans.userId,
+          trans.accCode
+        )
+      )
+    }
+  } catch (error) {
+    console.log('Error fetching data', error)
+  }
+  transactions.value = formattedData
+}
+</script>
 
 <template>
   <h2>Journal</h2>
+  <ul>
+    <li v-for="transaction in transactions" :key="transaction.id">
+      <TransactionCard :data="transaction" />
+    </li>
+  </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+ul {
+  padding: 0;
+}
+</style>
