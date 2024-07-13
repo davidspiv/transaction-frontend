a08c4b22f85036f1763cad227bc6a0c1bd2ce230
davidspiv <daspivack@gmail.com>
2024-07-12 18:05:31 -0700

    added primary account dropdown and placed dropdowns in a ul

--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -35,13 +35,29 @@ async function fetchTransactions() {
 
 <template>
   <h2>Journal</h2>
-  <select name="time-range" id="time-range">
-    <option value="all">All</option>
-    <option value="week">Week</option>
-    <option value="month">Month</option>
-    <option value="year-to-date">Year To Date</option>
-    <option value="year">Year</option>
-  </select>
+  <ul id="dropdown-filters">
+    <li>
+      <label for="time-range">Time Range</label>
+      <select name="time-range" id="time-range">
+        <option value="all">All</option>
+        <option value="week">Week</option>
+        <option value="month">Month</option>
+        <option value="year-to-date">Year To Date</option>
+        <option value="year">Year</option>
+      </select>
+    </li>
+    <li>
+      <label for="account">Primary Account</label>
+      <select name="account" id="account">
+        <option value="assets">Assets</option>
+        <option value="expenses">Expenses</option>
+        <option value="income">Income</option>
+        <option value="liabilities">Liabilities</option>
+        <option value="equity">Equity</option>
+      </select>
+    </li>
+  </ul>
+  <ul id="column-header"></ul>
   <ul>
     <li v-for="transaction in transactions" :key="transaction.id">
       <TransactionCard :data="transaction" />
@@ -50,6 +66,20 @@ async function fetchTransactions() {
 </template>
 
 <style scoped>
+#dropdown-filters {
+  display: flex;
+  gap: 3.5rem;
+}
+
+#dropdown-filters li {
+  display: flex;
+  flex-direction: column;
+}
+
+#column-header {
+  display: grid;
+}
+
 ul {
   padding: 0;
 }
