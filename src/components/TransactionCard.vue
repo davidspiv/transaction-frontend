f9becc618ae2a882d4db432727e1db86c7247101
davidspiv <daspivack@gmail.com>
2024-07-12 20:52:02 -0700

    styled checkbox

--- src/components/TransactionCard.vue
+++ src/components/TransactionCard.vue
@@ -11,7 +11,7 @@ const { memo, amount } = props.data
 <template>
   <tr>
     <td>{{ memo }}</td>
-    <td>omitTransaction</td>
+    <td><input type="checkbox" /></td>
     <td>affectedAccounts</td>
     <td>{{ amount }}</td>
   </tr>
@@ -22,5 +22,31 @@ td {
   padding: 2rem;
   border-top: 1px solid rgb(160 160 160);
   border-bottom: 1px solid rgb(160 160 160);
+}
+
+input[type='checkbox'] {
+  appearance: none;
+  -webkit-appearance: none;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  border: 1px solid rgb(160 160 160);
+  border-radius: 0.2rem;
+}
+
+input[type='checkbox']::before {
+  content: '';
+  width: 1rem;
+  height: 1rem;
+  transform: scale(0);
+  background-color: rgb(160 160 160);
+}
+
+input[type='checkbox']:checked::before {
+  transform: scale(1);
+}
+
+input[type='checkbox']:hover {
+  border: 1px solid white;
 }
 </style>
