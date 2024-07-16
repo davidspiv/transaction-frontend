6ba14afdcdf6dd0d474a71bbfa8b92ca57a2896b
davidspiv <daspivack@gmail.com>
2024-07-16 16:34:15 -0700

    fixed ts error by casting

--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -7,33 +7,34 @@ import type { Ref } from 'vue'
 const transactions: Ref<[] | Transaction[]> = ref([])
 
 const handler = (event?: Event) => {
-  let source: string
-  switch (event?.target.value) {
-    case 'year':
-      source =
-        'http://localhost:5000/api/transactions/?_time=year_account=all'
-      break
-    case 'year-to-date':
-      source =
-        'http://localhost:5000/api/transactions/?_time=year-to-date_account=all'
-      break
-    case 'month':
-      source =
-        'http://localhost:5000/api/transactions/?_time=month_account=all'
-      break
-    case 'week':
-      source =
-        'http://localhost:5000/api/transactions/?_time=week_account=all'
-      break
-    case 'day':
-      source =
-        'http://localhost:5000/api/transactions/?_time=day_account=all'
-      break
-    default:
-      source =
-        'http://localhost:5000/api/transactions/?_time=all_account=all'
-      break
+  let source: string =
+    'http://localhost:5000/api/transactions/?_time=all_account=all'
+  if (event && event.target) {
+    console.log(event)
+    switch ((event.target as HTMLSelectElement).value) {
+      case 'year':
+        source =
+          'http://localhost:5000/api/transactions/?_time=year_account=all'
+        break
+      case 'year-to-date':
+        source =
+          'http://localhost:5000/api/transactions/?_time=year-to-date_account=all'
+        break
+      case 'month':
+        source =
+          'http://localhost:5000/api/transactions/?_time=month_account=all'
+        break
+      case 'week':
+        source =
+          'http://localhost:5000/api/transactions/?_time=week_account=all'
+        break
+      case 'day':
+        source =
+          'http://localhost:5000/api/transactions/?_time=day_account=all'
+        break
+    }
   }
+
   fetchTransactions(source)
 }
 
