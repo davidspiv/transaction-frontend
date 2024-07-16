c5f08b639a7058f23f0d3dae74b7639c9e1dbffe
davidspiv <daspivack@gmail.com>
2024-07-16 16:16:48 -0700

    working dateRange

--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -11,27 +11,27 @@ const handler = (event?: Event) => {
   switch (event?.target.value) {
     case 'year':
       source =
-        'http://localhost:5000/api/transactions/?_time=year_account=all_limit=10'
+        'http://localhost:5000/api/transactions/?_time=year_account=all'
       break
     case 'year-to-date':
       source =
-        'http://localhost:5000/api/transactions/?_time=year-to-date_account=all_limit=10'
+        'http://localhost:5000/api/transactions/?_time=year-to-date_account=all'
       break
     case 'month':
       source =
-        'http://localhost:5000/api/transactions/?_time=month_account=all_limit=10'
+        'http://localhost:5000/api/transactions/?_time=month_account=all'
       break
     case 'week':
       source =
-        'http://localhost:5000/api/transactions/?_time=week_account=all_limit=10'
+        'http://localhost:5000/api/transactions/?_time=week_account=all'
       break
     case 'day':
       source =
-        'http://localhost:5000/api/transactions/?_time=day_account=all_limit=10'
+        'http://localhost:5000/api/transactions/?_time=day_account=all'
       break
     default:
       source =
-        'http://localhost:5000/api/transactions/?_time=all_account=all_limit=10'
+        'http://localhost:5000/api/transactions/?_time=all_account=all'
       break
   }
   fetchTransactions(source)
