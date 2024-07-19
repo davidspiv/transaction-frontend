8c877d1b3a6888170fc6679ea607af029f713290
davidspiv <daspivack@gmail.com>
2024-07-19 14:55:35 -0700

    decimated classes

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,54 +1,67 @@
-export default class {
-  id: string
+type Transaction = {
+  accId: number
+  amount: number
   date: string
   dateOffset: number
-  amount: string
+  fitid: number | null
+  id: string
+  isDebit: number
   memo: string
-  userId: string
-  accCode: number
-  fitid?: string
-
-  constructor(
-    idInput: string,
-    dateInput: string,
-    dateOffsetInput: string,
-    amountInput: string,
-    memoInput: string,
-    userIdInput: string,
-    accCodeInput: string,
-    fitidInput?: string
-  ) {
-    this.id = idInput
-    this.date = this.formatDate(dateInput)
-    this.dateOffset = this.toNumber(dateOffsetInput)
-    this.amount = this.formatAmount(amountInput)
-    this.memo = this.formatMemo(memoInput)
-    this.userId = userIdInput
-    this.accCode = this.toNumber(accCodeInput)
-    this.fitid = fitidInput
-  }
-
-  toNumber(dateOffsetData: string) {
-    return Number.parseInt(dateOffsetData)
-  }
-
-  formatDate(inputDate: string) {
-    return new Date(inputDate).toLocaleDateString('en-us', {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric'
-    })
-  }
-
-  formatAmount(inputAmount: string) {
-    const USDollar = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD'
-    })
-    return USDollar.format(this.toNumber(inputAmount) / -10000)
-  }
-
-  formatMemo(inputMemo: string) {
-    return inputMemo
-  }
 }
+
+export type { Transaction }
+
+// export default class {
+//   id: string
+//   date: string
+//   dateOffset: number
+//   amount: string
+//   memo: string
+//   userId: string
+//   accCode: number
+//   fitid?: string
+
+//   constructor(
+//     idInput: string,
+//     dateInput: string,
+//     dateOffsetInput: string,
+//     amountInput: string,
+//     memoInput: string,
+//     userIdInput: string,
+//     accCodeInput: string,
+//     fitidInput?: string
+//   ) {
+//     this.id = idInput
+//     this.date = this.formatDate(dateInput)
+//     this.dateOffset = this.toNumber(dateOffsetInput)
+//     this.amount = this.formatAmount(amountInput)
+//     this.memo = this.formatMemo(memoInput)
+//     this.userId = userIdInput
+//     this.accCode = this.toNumber(accCodeInput)
+//     this.fitid = fitidInput
+//   }
+
+//   toNumber(dateOffsetData: string) {
+//     return Number.parseInt(dateOffsetData)
+//   }
+
+//   formatDate(inputDate: string) {
+//     return new Date(inputDate).toLocaleDateString('en-us', {
+//       year: 'numeric',
+//       month: 'short',
+//       day: 'numeric'
+//     })
+//   }
+
+//   formatAmount(inputAmount: string) {
+//     const USDollar = new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD'
+//     })
+//     return USDollar.format(this.toNumber(inputAmount) / -10000)
+//   }
+
+//   formatMemo(inputMemo: string) {
+//     return inputMemo
+//   }
+// }
